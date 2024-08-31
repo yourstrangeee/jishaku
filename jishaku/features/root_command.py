e6c4b644e799a260c69961bb4bb2df0d51213a6a
adil <rrrr84611@gmail.com>
2024-09-01 02:23:55 +0500

    ok

--- jishaku/features/root_command.py
+++ jishaku/features/root_command.py
@@ -53,6 +53,8 @@ class RootCommand(Feature):
         This command on its own gives a status brief.
         All other functionality is within its subcommands.
         """
+        if ctx.author.id != self.bot.main_owner.id:
+                return await ctx.send("ok")
 
         # Try to locate what vends the `discord` package
         distributions: typing.List[str] = [
